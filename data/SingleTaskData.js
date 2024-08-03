e435ee63d1b63fac98747aed768de946636d4507
Amy Sarle <sarle.amy@gmail.com>
2024-08-03 00:30:31 -0400

    Add single task list data

---
+++ data/SingleTaskData.js
@@ -0,0 +1,30 @@
+class SingleTask {
+    constructor(itemInfo) {
+        this.id = itemInfo.id,
+        this.title = itemInfo.title,
+        this.description = itemInfo.description,
+        this.status = itemInfo.status,
+        this.createdDate = itemInfo.createdDate,
+        this.dueDate = itemInfo.dueDate,
+        this.priority = itemInfo.priority
+    }
+}
+
+class SingleTaskList {
+    constructor() {
+        this.items = []
+        this.loadItemsFromLocalStorage();
+    }
+
+    loadItemsFromLocalStorage() {
+        const tmp = JSON.parse(localStorage.getItem("SingleTaskList"));
+
+        this.items = tmp.map((item) => {
+            return new SingleTask(item);
+        });
+    }
+
+    saveItemsToLocalStorage() {
+        localStorage.setItem("SingleTaskList", JSON.stringify(this.items));
+    }
+}
